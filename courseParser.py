c9c6ed97b279ffe5b950650fa2680cbbdfddaca6
Peter Bi <peter@Peters-MacBook-Pro-2.local>
2017-02-23 10:05:50 -0600

    parsed all EECS course data from JSON format to lisp/LTRE engine recognized fact format, including course info and meeting time. Yet to encode pre-requisite constraints

---
+++ courseParser.py
@@ -0,0 +1,46 @@
+#python courseParser that parse the class data of EECS department into LTRE fact format
+# (rassert! (course catalog_num course_id (title) (topic)))
+# (rassert! (meetingTime course_id (meetingDays) startHr endHr startMin EndMin))
+import requests
+import json
+
+params = {
+  'key': '6SCzp7Xb8sgPELO1'
+}
+response = requests.get('http://api.asg.northwestern.edu/courses/?key=6SCzp7Xb8sgPELO1&term=4660&subject=EECS')
+# calling response.json() returns the response as a Python dictionary or list
+content= response.json()
+
+print len(content)
+#open the file
+target = open('courses.lsp', 'w')
+
+for i in range(0, len(content)):
+        
+	#parse the fact: time
+        meetingDays = "("
+        
+	#if course meeting days do not exist, skip this course
+	if content[i]['meeting_days'] is None :
+		continue
+	j = 0
+        while (j <  len(content[i]['meeting_days'])):
+                meetingDays =meetingDays + content[i]['meeting_days'][j:j+2]+ ' '
+                j = j + 2;
+        meetingDays = meetingDays + ")"
+
+        #parse the meeting hr and min
+        startHr = str(content[i]['start_time'])[:2]
+        startMin = str(content[i]['start_time'])[3:]
+        endHr = str(content[i]['end_time'])[:2]
+        endMin = str(content[i]['end_time'])[3:]
+
+        target.write('(rassert! (meetingTime '+  str(content[i]['course_id'])+ ' '+ meetingDays + ' ' + startHr + ' ' + startMin + ' '+ endHr+ ' ' + endMin+'))')
+
+	target.write('\n')
+
+	#parse the fact for course 
+	target.write('(rassert! (course ' + str(content[i]['catalog_num']) + ' ' + str(content[i]['course_id'])+ ' (' + str(content[i]['title']) + ') (' + str(content[i]['topic']) + ')))')	
+	target.write('\n') 
+
+target.close()
